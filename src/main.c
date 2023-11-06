4350bd7c590374c83825457c1bca9244f1d38ab2
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2023-11-06 15:45:57 +0100

    read binary file to memory

---
+++ src/main.c
@@ -0,0 +1,24 @@
+
+#include "mem.h"
+#include "stdio.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("ERROR: No input file provided\n");
+        exit(1);
+    }
+
+    const char *fname = argv[1];
+
+    FILE *f = fopen(fname, "rb");
+
+    if (f == NULL)
+    {
+        printf("ERROR: Could not open file %s\n", fname);
+        exit(1);
+    }
+
+    const size_t fsize = fread(mem, sizeof(mem), 1, f);
+}
